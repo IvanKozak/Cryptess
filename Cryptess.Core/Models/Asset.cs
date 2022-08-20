944d1762af77a55a34c625acb28afadb315fddc3
Ivan Kozak <2.vanya.k@gmail.com>
2022-08-20 12:41:58 +0300

    Add Asset model

---
+++ Cryptess.Core/Models/Asset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cryptess.Core.Models
+{
+    public class Asset
+    {
+        public string AssetId { get; set; }
+        public string Name { get; set; }
+        public decimal price { get; set; }
+        public decimal Volume24h { get; set; }
+        public decimal Change1h { get; set; }
+        public decimal Change24h { get; set; }
+        public decimal Change7d { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}

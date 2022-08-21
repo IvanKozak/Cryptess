8056cb31c2683950e9a6977a9468a2cbe17739fa
Banderarius <2.vanya.k@gmail.com>
2022-08-21 17:38:05 +0300

    Add ViewModel and View for main page
    View shows 100 currency entries each consisting of id and name.

--- Cryptess.Core/ViewModels/CurrOverviewViewModel.cs
+++ Cryptess.Core/ViewModels/CurrOverviewViewModel.cs
@@ -1,8 +1,34 @@
-﻿using MvvmCross.ViewModels;
+﻿using Cryptess.Core.Models;
+using Cryptess.Core.Repositories;
+using MvvmCross.ViewModels;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Cryptess.Core.ViewModels
 {
     public class CurrOverviewViewModel : MvxViewModel
     {
+        private readonly IAssetRepository _assetRepo;
+
+        public CurrOverviewViewModel(IAssetRepository assetRepo)
+        {
+            _assetRepo = assetRepo;
+        }
+
+        private ObservableCollection<SimpleAsset> _assets = new ObservableCollection<SimpleAsset>();
+
+        public ObservableCollection<SimpleAsset> Assets
+        {
+            get { return _assets; }
+            set
+            {
+                SetProperty(ref _assets, value);
+            }
+        }
+
+        public override async Task Initialize()
+        {
+            _assets = _assetRepo.GetAssetsOverview();
+        }
     }
 }

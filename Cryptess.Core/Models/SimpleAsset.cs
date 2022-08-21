72d27e859020a1fdbf7e80f2ba339319f64fa9e1
Banderarius <2.vanya.k@gmail.com>
2022-08-21 17:31:43 +0300

    Add SimpleAsset model

---
+++ Cryptess.Core/Models/SimpleAsset.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Cryptess.Core.Models
+{
+    public class SimpleAsset
+    {
+        [JsonPropertyName("asset_id")]
+        public string AssetId { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}

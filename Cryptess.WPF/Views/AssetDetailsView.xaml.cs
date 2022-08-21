ee6a07a6a3ebfbde9e238e3dcfb24ed0d119b368
Banderarius <2.vanya.k@gmail.com>
2022-08-22 00:12:50 +0300

    Add AssetDetailsView

---
+++ Cryptess.WPF/Views/AssetDetailsView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Cryptess.Core.ViewModels;
+using MvvmCross.Platforms.Wpf.Presenters.Attributes;
+using MvvmCross.Platforms.Wpf.Views;
+using MvvmCross.ViewModels;
+
+namespace Cryptess.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for AssetDetailsView.xaml
+    /// </summary>
+    [MvxContentPresentation]
+    [MvxViewFor(typeof(AssetDetailsViewModel))]
+    public partial class AssetDetailsView : MvxWpfView
+    {
+        public AssetDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}

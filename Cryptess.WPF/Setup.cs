89a3c684095cc42b456af33afdc4bcc7967deaad
Banderarius <2.vanya.k@gmail.com>
2022-08-21 17:30:58 +0300

    Add configuration and wire it up to appsettings

--- Cryptess.WPF/Setup.cs
+++ Cryptess.WPF/Setup.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Cryptess.Core.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MvvmCross;
+using MvvmCross.IoC;
 using MvvmCross.Platforms.Wpf.Core;
 using Serilog;
 using Serilog.Extensions.Logging;
+using System.IO;
 
 namespace Cryptess.WPF
 {
     public class Setup : MvxWpfSetup<Core.App>
     {
-
         protected override ILoggerFactory? CreateLogFactory()
         {
             Log.Logger = new LoggerConfiguration()
@@ -17,10 +21,27 @@ namespace Cryptess.WPF
 
             return new SerilogLoggerFactory();
         }
-
         protected override ILoggerProvider? CreateLogProvider()
         {
             return new SerilogLoggerProvider();
+        }
+        protected override void InitializeFirstChance(IMvxIoCProvider iocProvider)
+        {
+            base.InitializeFirstChance(iocProvider);
+            Mvx.IoCProvider.RegisterSingleton<IConfiguration>(AddConfiguration());
+            Mvx.IoCProvider.RegisterType<IAssetRepository, DummyAssetRepository>();
+        }
+        private IConfiguration AddConfiguration()
+        {
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+#if DEBUG
+            configurationBuilder.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+#else
+            configurationBuilder.AddJsonFile("appsettings.Production.json", optional: true, reloadOnChange: true);
+#endif
+            return configurationBuilder.Build();
         }
     }
 }

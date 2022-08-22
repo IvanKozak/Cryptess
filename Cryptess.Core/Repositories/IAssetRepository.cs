51b85c0d0439103de265788787a110166a33fda1
Banderarius <2.vanya.k@gmail.com>
2022-08-22 17:59:23 +0300

    Add asyncronous wariants of methods to  IAssetRepository
    MvvmCross has an issue that leads to deadlock when asyncronous call is awaited in Initialize() method of the ViewModel that is registered in AppStart. Therefore temporarily we need syncronous variants to call in that ViewModel.

--- Cryptess.Core/Repositories/IAssetRepository.cs
+++ Cryptess.Core/Repositories/IAssetRepository.cs
@@ -1,12 +1,17 @@
 ﻿using Cryptess.Core.Models;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 namespace Cryptess.Core.Repositories
 {
     public interface IAssetRepository
     {
         ObservableCollection<Asset> GetAssets();
+        Task<ObservableCollection<Asset>> GetAssetsAsync();
         ObservableCollection<SimpleAsset> GetAssetsOverview();
+        Task<ObservableCollection<SimpleAsset>> GetAssetsOverviewAsync();
         Asset GetAssetById(string assetId);
+        Task<Asset> GetAssetByIdAsync(string assetId);
+
     }
 }

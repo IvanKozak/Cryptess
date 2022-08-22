136f6cfbca82fc1e6d0a9ab33edf4c8c5ffcca74
Banderarius <2.vanya.k@gmail.com>
2022-08-22 21:19:15 +0300

    Add class for retrieving assets data from CryptingUp API
    Added some basic integration tests

---
+++ Cryptess.Core/Repositories/CryptingUpAssetRepository.cs
@@ -0,0 +1,126 @@
+﻿using Cryptess.Core.Models;
+using Microsoft.Extensions.Configuration;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Cryptess.Core.Repositories
+{
+    public class CryptingUpAssetRepository : IAssetRepository
+    {
+        private readonly HttpClient _client = new HttpClient();
+        private readonly IConfiguration _config;
+
+        public CryptingUpAssetRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Asset GetAssetById(string assetId)
+        {
+            string requestUri = $"https://cryptingup.com/api/assets/{assetId}";
+            var task = Task.Run(() =>
+            {
+                return _client.GetStringAsync(requestUri);
+            });
+            task.Wait();
+            var jsonString = task.Result;
+            using (JsonDocument document = JsonDocument.Parse(jsonString))
+            {
+                return document.RootElement.GetProperty("asset").Deserialize<Asset>();
+            }
+        }
+
+        public async Task<Asset> GetAssetByIdAsync(string assetId)
+        {
+            string requestUri = $"https://cryptingup.com/api/assets/{assetId}";
+            var jsonString = await _client.GetStringAsync(requestUri);
+            using (JsonDocument document = JsonDocument.Parse(jsonString))
+            {
+                return document.RootElement.GetProperty("asset").Deserialize<Asset>();
+            }
+        }
+
+        public ObservableCollection<Asset> GetAssets()
+        {
+            int size;
+            var sizeString = _config["CryptingUp:AssetsSize"];
+            if (int.TryParse(sizeString, out size) == false)
+            {
+                size = 10;
+            }
+            string requestUri = $"https://cryptingup.com/api/assets?size={size}";
+            var task = Task.Run(() =>
+            {
+                return _client.GetStringAsync(requestUri);
+            });
+            task.Wait();
+            var jsonString = task.Result;
+            using (JsonDocument document = JsonDocument.Parse(jsonString))
+            {
+                JsonElement root = document.RootElement;
+                JsonElement assetsElement = root.GetProperty("assets");
+                return assetsElement.Deserialize<ObservableCollection<Asset>>();
+            }
+        }
+
+        public async Task<ObservableCollection<Asset>> GetAssetsAsync()
+        {
+            int size;
+            var sizeString = _config["CryptingUp:AssetsSize"];
+            if (int.TryParse(sizeString, out size) == false)
+            {
+                size = 10;
+            }
+
+            string requestUri = $"https://cryptingup.com/api/assets?size={size}";
+            var jsonString = await _client.GetStringAsync(requestUri);
+            using (JsonDocument document = JsonDocument.Parse(jsonString))
+            {
+                JsonElement root = document.RootElement;
+                JsonElement assetsElement = root.GetProperty("assets");
+                return assetsElement.Deserialize<ObservableCollection<Asset>>();
+            }
+        }
+
+        public ObservableCollection<SimpleAsset> GetAssetsOverview()
+        {
+            int size;
+            var sizeString = _config["CryptingUp:AssetsSize"];
+            if (int.TryParse(sizeString, out size) == false)
+            {
+                size = 10;
+            }
+            var requestUri = $"https://cryptingup.com/api/assets?size={size}";
+            var task = Task.Run(() => _client.GetStringAsync(requestUri));
+            task.Wait();
+            var jsonString = task.Result;
+            using (JsonDocument document = JsonDocument.Parse(jsonString))
+            {
+                JsonElement root = document.RootElement;
+                JsonElement assetsElement = root.GetProperty("assets");
+                return assetsElement.Deserialize<ObservableCollection<SimpleAsset>>();
+            }
+
+        }
+
+        public async Task<ObservableCollection<SimpleAsset>> GetAssetsOverviewAsync()
+        {
+            int size;
+            var sizeString = _config["CryptingUp:AssetsSize"];
+            if (int.TryParse(sizeString, out size) == false)
+            {
+                size = 10;
+            }
+            var url = $"https://cryptingup.com/api/assets?size={size}";
+            var jsonString = await _client.GetStringAsync(url);
+            using (JsonDocument document = JsonDocument.Parse(jsonString))
+            {
+                JsonElement root = document.RootElement;
+                JsonElement assetsElement = root.GetProperty("assets");
+                return assetsElement.Deserialize<ObservableCollection<SimpleAsset>>();
+            }
+        }
+    }
+}
---
+++ Cryptess.Tests/Integration/CryptingUpAssetRepositoryTests.cs
@@ -0,0 +1,68 @@
+﻿using Cryptess.Core.Repositories;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace Cryptess.Tests.Integration
+{
+    public class CryptingUpAssetRepositoryTests
+    {
+        private readonly IAssetRepository _assetRepo;
+        private Mock<IConfiguration> _config;
+        public CryptingUpAssetRepositoryTests()
+        {
+            _config = new Mock<IConfiguration>();
+            _config.Setup(c => c["CryptingUp:AssetsSize"])
+                   .Returns("10");
+            _assetRepo = new CryptingUpAssetRepository(_config.Object);
+        }
+
+        [Fact]
+        public void GetAssetsOverview_ShouldReturnRightAmountOfObjects()
+        {
+            var assets = _assetRepo.GetAssetsOverview();
+
+            assets.Count.Should().Be(10);
+        }
+
+        [Fact]
+        public async Task GetAssetsOverviewAsync_ShouldReturnRightAmountOfObjects()
+        {
+            var assets = await _assetRepo.GetAssetsOverviewAsync();
+
+            assets.Count.Should().Be(10);
+        }
+
+        [Fact]
+        public void GetAssets_ShouldReturnRightAmountOfObjects()
+        {
+            var assets = _assetRepo.GetAssets();
+
+            assets.Count.Should().Be(10);
+        }
+
+        [Fact]
+        public async Task GetAssetsAsync_ShouldReturnRightAmountOfObjects()
+        {
+            var assets = await _assetRepo.GetAssetsAsync();
+
+            assets.Count.Should().Be(10);
+        }
+
+        [Fact]
+        public async Task GetAssetByIdAsync_ShouldReturnRightObject()
+        {
+            var asset = await _assetRepo.GetAssetByIdAsync("BTC");
+
+            asset.Name.Should().Be("Bitcoin");
+        }
+
+        [Fact]
+        public void GetAssetById_ShouldReturnRightObject()
+        {
+            var asset = _assetRepo.GetAssetById("BTC");
+
+            asset.Name.Should().Be("Bitcoin");
+        }
+    }
+}

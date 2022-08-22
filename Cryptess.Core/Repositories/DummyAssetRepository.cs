76d93b143d582800ce93cd46e5c8f992482a6554
Banderarius <2.vanya.k@gmail.com>
2022-08-22 18:01:02 +0300

    Add async methods to DummyAssetRepository

--- Cryptess.Core/Repositories/DummyAssetRepository.cs
+++ Cryptess.Core/Repositories/DummyAssetRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Cryptess.Core.Repositories
 {
@@ -36,6 +37,27 @@ namespace Cryptess.Core.Repositories
             }
         }
 
+        public async Task<Asset> GetAssetByIdAsync(string assetId)
+        {
+            string path = _config["DummyAssetsPath"];
+            using (FileStream stream = File.OpenRead(path))
+            {
+                using (JsonDocument document = await JsonDocument.ParseAsync(stream))
+                {
+                    JsonElement root = document.RootElement;
+                    JsonElement assetsElement = root.GetProperty("assets");
+                    foreach (var asset in assetsElement.EnumerateArray())
+                    {
+                        if (asset.GetProperty("asset_id").ToString() == assetId)
+                        {
+                            return asset.Deserialize<Asset>();
+                        }
+                    }
+                    return null;
+                }
+            }
+        }
+
         public ObservableCollection<Asset> GetAssets()
         {
             string path = _config["DummyAssetsPath"];
@@ -49,12 +71,41 @@ namespace Cryptess.Core.Repositories
                 }
             }
         }
+
+        public async Task<ObservableCollection<Asset>> GetAssetsAsync()
+        {
+            string path = _config["DummyAssetsPath"];
+            using (FileStream stream = File.OpenRead(path))
+            {
+                using (JsonDocument document = await JsonDocument.ParseAsync(stream))
+                {
+                    JsonElement root = document.RootElement;
+                    JsonElement assetsElement = root.GetProperty("assets");
+                    return assetsElement.Deserialize<ObservableCollection<Asset>>();
+                }
+            }
+        }
+
         public ObservableCollection<SimpleAsset> GetAssetsOverview()
         {
             string filePath = _config["DummyAssetsPath"];
             using (var sourceStream = File.OpenRead(filePath))
             {
                 using (JsonDocument document = JsonDocument.Parse(sourceStream))
+                {
+                    JsonElement root = document.RootElement;
+                    JsonElement assetsElement = root.GetProperty("assets");
+                    return assetsElement.Deserialize<ObservableCollection<SimpleAsset>>();
+                }
+            }
+        }
+
+        public async Task<ObservableCollection<SimpleAsset>> GetAssetsOverviewAsync()
+        {
+            string filePath = _config["DummyAssetsPath"];
+            using (var sourceStream = File.OpenRead(filePath))
+            {
+                using (JsonDocument document = await JsonDocument.ParseAsync(sourceStream))
                 {
                     JsonElement root = document.RootElement;
                     JsonElement assetsElement = root.GetProperty("assets");
